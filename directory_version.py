ab671fca9173ee046f6a387c770bbfe4fbb5cc43
wynter <gusdud0122@kookmin.ac.kr>
2022-04-21 22:22:51 +0900

    modify : find x_second_idx in reverse

--- directory_version.py
+++ directory_version.py
@@ -28,6 +28,8 @@ def readDirectory():
 
     except:
         resultF.close()
+        print(filename)
+        traceback.print_exc()
         messagebox.showwarning("warning", "please select directory")
 
 
@@ -44,56 +46,43 @@ def analyzeData(path, filename, interval, pw, resultF):
 
     # calculate rate of change
     for i in range(len(copyData)-1):
-        h = 0.000100001
+        h = copyData[1][0]
         f_0 = copyData[i][1]
         f_1 = copyData[i+1][1]
         copyData[i][2] = (f_1 - f_0)/h
 
     # find five values in small order
-    min_array = copyData.copy()
-    min_array = min_array[min_array[:, 2].argsort()]
-    min_array = min_array[:5]
-
-    # choose the latest value of min array
-    # its rate of change value must be smaller than -0.2
-    time_temp = 0.0000000
-    min_rate_of_change = 0.0
-    for i in range(len(min_array)):
-        if min_array[i][2] < -0.2 and min_array[i][0] > time_temp:
-            time_temp = min_array[i][0]
-            min_rate_of_change = min_array[i][2]
-
-    # find index of the latest pulse in copy data
-    min_array_idx = np.where((copyData[:, 2] == min_rate_of_change))
-    x_second_idx = min_array_idx[0][0]
-    x_second = float(x_second_idx)*(1/1000) + \
-        float(x_second_idx)*(1/1000000000)
-    x_second_value = copyData[x_second_idx][1]
-
-    # calculate interval and pulse width
-    cal_interval = float(interval) * (1/1000) + \
-        float(interval) * (1/1000000000)
-    pw = float(pw) * (1/1000) + float(pw) * (1/1000000000)
+    for i in range(len(copyData)-1, -1, -1):
+        if copyData[i][2] < -0.2:
+            x_second_idx = i
+            break
 
     # find time and index of first pulse
-    subtract = cal_interval + pw
-    x_first = x_second - subtract
-    x_fisrt_idx = np.where((copyData[:, 0]) == round(x_first, 10))
+    subtract = int(interval) + int(pw)
+    subtract_round = subtract / (round(h, 7)*1000)
+    
+    x_first_idx = int(x_second_idx - subtract_round)
+    
     try:
-        x_fisrt_idx = x_fisrt_idx[0][0]
-        x_first_value = copyData[x_fisrt_idx][1]
-        ## first_rate_of_change = copyData[x_fisrt_idx][2]
-        get_ratio = x_second_value / x_first_value
-        resultF.write(str(interval) + ' ' + str(x_first_value) +
-                      ' ' + str(x_second_value) + ' ' + str(get_ratio) + '\n')
+        x_first_time = copyData[x_first_idx][0]
+        x_second_time = copyData[x_second_idx][0]
+        x_first_value = copyData[x_first_idx][1]
+        x_second_value = copyData[x_second_idx][1]
+        ppf = x_second_value / x_first_value
+        
+        resultF.write(str(interval) + '    ' + str(x_first_time) +
+                      '    ' + str(x_second_time) + '    ' + str(ppf) + '\n')
 
     except:
+        print(filename)
+        traceback.print_exc()
         resultF.write(filename + ' : !!!!! Analyze failed !!!!!\n')
+
 
 
 # GUI
 window = Tk()
-window.title("이동준 돼지")
+window.title("PPF_comb_20220421")
 window.geometry("400x200")
 
 
